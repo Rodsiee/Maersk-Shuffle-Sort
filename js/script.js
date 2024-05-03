5a18ad953849147d10e50ef28ef8a5d8d8fc5d64
Rodsiee <168813114+Rodsiee@users.noreply.github.com>
2024-05-03 18:20:39 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const cardContainer = document.querySelector('.container');
+const shuffleBtn = document.getElementById('shuffleBtn');
+const sortBtn = document.getElementById('sortBtn');
+
+const cards = [];
+
+const createCards = () => {
+    for (let i = 1; i <= 9; i++) {
+        const card = document.createElement('div');
+        card.classList.add('card-item', `card-${i}`);
+        card.innerHTML = i;
+        cards.push(card);
+    }
+};
+createCards();
+cards.forEach(card => cardContainer.appendChild(card));
+
+const shuffleCards = () => {
+    cards.sort(() => Math.random() - 0.5)
+        .forEach(card => cardContainer.appendChild(card));
+};
+
+const sortCards = () => {
+    cards.sort((a, b) => parseInt(a.innerHTML) - parseInt(b.innerHTML))
+        .forEach(card => cardContainer.appendChild(card));
+};
+
+shuffleBtn.addEventListener('click', shuffleCards);
+sortBtn.addEventListener('click', sortCards);
+const name = "Rodali";
+document.getElementById('footerName').innerHTML = `Shuffle and sort by ${name}`;
